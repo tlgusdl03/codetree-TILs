4558501bbee8eddaef11fcc57ccf292ade21a88f
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-01 22:28:28 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240501/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240501/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int n,x,y,dir=0,count=1;
+int n,x,y,dir=2;
 int dx[4]={0,1,0,-1};
 int dy[4]={1,0,-1,0};
 int a[100][100];
@@ -11,22 +11,24 @@ bool inRange(int x, int y, int n){
 
 int main() {
     cin>>n;
-    x = n/2;
-    y = n/2;
+    int count = n * n;
+    x = n-1;
+    y = x;
+
     for(int i=0;i < n * n;i++){
         a[x][y] = count;
-
         int nx = x + dx[dir];
         int ny = y + dy[dir];
+
         if(!inRange(nx, ny, n) || a[nx][ny] != 0){
-            dir = (dir - 1 + 4) % 4;
+            dir = (dir + 1) % 4;
             nx = x + dx[dir];
             ny = y + dy[dir];
         }
 
         x = nx;
         y = ny;
-        count++;
+        count--;
     }
 
     for(int i=0;i<n;i++){
