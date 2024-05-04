94c5cb1e0d69c508c8ff2037fb3a78631354c5db
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-04 19:54:30 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240504/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int N, K;
+int answer[8];
+
+void choose(int current_index){
+    if(current_index == N){
+        for(int i=0;i<N;i++){
+            cout<<answer[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+    else{
+        for(int i=1;i<=K;i++){
+            if(current_index != 0 && answer[0] == answer[1] && answer[i] == i){
+                
+            }
+            else{
+                answer[current_index]=i;
+                choose(current_index+1);
+            }
+        }
+    }
+}
+int main() {
+    cin>>K>>N;
+    choose(0);
+    return 0;
+}
