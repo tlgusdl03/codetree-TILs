ca93c7c5298db7b6838e01a32fac0c1399d116f3
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-04 19:39:06 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 58ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240504/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int N, K;
+int answer[8];
+
+void choose(int current_index){
+    if(current_index == N){
+        for(int i=0;i<N;i++){
+            cout<<answer[i]<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+    else{
+        for(int i=1;i<=K;i++){
+            answer[current_index]=i;
+            choose(current_index+1);
+        }
+    }
+}
+int main() {
+    cin>>K>>N;
+    choose(0);
+    return 0;
+}
