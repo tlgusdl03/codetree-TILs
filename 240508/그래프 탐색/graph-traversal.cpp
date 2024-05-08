3dbbbfc6f4f8fbab8dfdb478b68db836a37dccf2
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-08 19:45:49 +0900

    Title: 그래프 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240508/그래프 탐색/graph-traversal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> graph[1001];
+int N, M, count;
+bool isVisited[1001];
+
+void DFS(int vertex){
+    for(int i=0; i<graph[vertex].size(); i++){
+        int current_v = graph[vertex][i];
+        if(!isVisited[current_v]){
+            count++;
+            isVisited[current_v] = true;
+            DFS(current_v);
+        }
+    }
+}
+
+int main() {
+    int x, y;
+    cin>>N>>M;
+    for(int i=0;i<M;i++){
+        cin>>x>>y;
+        graph[x].push_back(y);
+    }
+
+    DFS(1);
+
+    cout<<count<<'\n';
+    return 0;
+}
