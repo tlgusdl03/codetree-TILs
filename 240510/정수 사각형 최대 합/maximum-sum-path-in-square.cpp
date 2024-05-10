80d888e478bbc0e801ade1fd4c1852d4c6329560
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-10 21:02:21 +0900

    Title: 정수 사각형 최대 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240510/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX 100
+
+int n;
+int num[MAX][MAX];
+int dp[MAX][MAX];
+
+void init(){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> num[i][j];
+        }
+    }
+ 
+    dp[0][0] = num[0][0];
+    
+    for(int i = 1; i < n; i++){
+        dp[0][i] = dp[0][i-1] + num[0][i]; 
+    }
+    for(int i = 1; i < n; i++){
+        dp[i][0] = dp[i-1][0] + num[i][0];
+    }
+
+    for(int i = 2; i < n; i++){
+        for(int j = 1; j < n; j++){
+            dp[i][j] = max(dp[i - 1][j], dp[i][j-1]) + num[i][j];
+        }
+    }
+}
+
+int main() {
+    cin>>n;
+
+    init();
+    cout<<dp[0][n-1]<<'\n';
+    return 0;
+}
