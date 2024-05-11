09bc30f9b61fa9088e2d1b172634980cc98c527b
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2024-05-11 14:52:02 +0900

    Title: 컨베이어 벨트, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/컨베이어 벨트/conveyor-belt.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#define MAX_LEN 400
+using namespace std;
+
+int input[MAX_LEN];
+int n, t;
+
+void simulate(){
+    int last_index = 2 * n - 1; 
+    int temp = input[last_index];
+
+    for(int i=0;i<t;i++){
+        for(int j=2*n-1; j>=1; j--){
+            input[j] = input[j-1];
+        }
+        input[0] = temp;
+    }
+}
+
+int main(){
+    cin>>n>>t;
+
+    for(int i=0; i<2*n; i++){
+        cin>>input[i];
+    }
+
+    simulate();
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<n; j++){
+            cout<<input[i * n + j]<<' ';
+        }
+        cout<<'\n';
+    }
+
+    return 0;
+}
