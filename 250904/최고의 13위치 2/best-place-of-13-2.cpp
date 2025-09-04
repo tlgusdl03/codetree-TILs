05cce7a4da8fce67d2a55ea7b1ef9ed5abe60103
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-04 18:13:39 +0900

    Title: 최고의 13위치 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250904/최고의 13위치 2/best-place-of-13-2.cpp
+++ 250904/최고의 13위치 2/best-place-of-13-2.cpp
@@ -11,10 +11,11 @@ int main() {
         for (int j = 0; j < N; j++) cin >> arr[i][j];
 
     int result = 0;
-    for (int i = 0; i < N - 1; i++){
-        for (int j = i + 1; j < N; j++){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
             for (int k = 0; k < N - 2; k++){
                 for (int l = 0; l < N - 2; l++){
+                    if (i == j && abs(k - l) < 2) continue;
                     int temp = arr[i][k] + arr[i][k + 1] + arr[i][k + 2] + arr[j][l] + arr[j][l + 1] + arr[j][l + 2];
                     result = max(result, temp);
                 }
