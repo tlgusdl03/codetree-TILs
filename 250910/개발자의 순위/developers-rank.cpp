c4114d52bbffa74dafc78e98edb234e35438b44d
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-10 17:37:18 +0900

    Title: 개발자의 순위, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250910/개발자의 순위/developers-rank.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <set>
+#include <tuple>
+using namespace std;
+
+int c, n;
+int arr[10][20];
+int results[20][20] = {0,};
+int main() {
+    cin >> c >> n;
+
+    for (int i = 0; i < c; i++)
+        for (int j = 0; j < n; j++) cin >> arr[i][j];
+
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            for (int k = j + 1; k < n; k++) {
+                results[arr[i][j]][arr[i][k]]++;
+            }
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++){
+            if(results[i][j] != 0 && results[j][i] == 0) count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
