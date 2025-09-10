e11009c4a427c18b17a94bd5d7be1be7c6ebb346
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-10 17:45:21 +0900

    Title: 개발자의 순위, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250910/개발자의 순위/developers-rank.cpp
+++ 250910/개발자의 순위/developers-rank.cpp
@@ -23,7 +23,7 @@ int main() {
     int count = 0;
     for (int i = 0; i < 20; i++) {
         for (int j = 0; j < 20; j++){
-            if(results[i][j] != 0 && results[j][i] == 0) count++;
+            if(i != j && results[i][j] == c) count++;
         }
     }
 
