76826ab70ee3c04445c12e938192c9746e45abd6
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-11 16:59:21 +0900

    Title: 상해버린 치즈, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250911/상해버린 치즈/rotten-cheese.cpp
+++ 250911/상해버린 치즈/rotten-cheese.cpp
@@ -4,39 +4,31 @@
 #include <vector>
 using namespace std;
 
-/* 
-사람의 수 : N
-치즈의 종류: M
-치즈를 먹은 기록의 수 : D
-아픈 기록의 수 : S
+struct EatLogs {
+    int p, m, t;
+};
 
-N개의 기록
-( p, m, t )
-
-S개의 기록
-( p, t )
-
-여러 사람이 1 종류의 치즈를 여러번 먹을 수 있음
-한 사람이 여러 종류의 치즈를 먹을 수 있음
-
-S개의 데이터를 이용하여 p가 t 이전에 먹은 치즈를 상한 치즈로 규정
-이후 상한 치즈를 먹은 사람을 추척
-
-1-base의 2차원 배열을 이용하여 누가 언제 어떤 치즈를 먹었는지 관리
-*/
-
-struct SickData {
+struct SickLogs {
     int p, t;
 };
 
 int N, M, D, S;
-vector<SickData> sickDatas;
-// 치즈 섭취 관리 배열
-int eatDatas[51][51] = {0, };
-// 상한 치즈 목록
-set<int> rottenCheese;
-// 아픈 사람 목록
+
+vector<EatLogs> eatLogs;
+vector<SickLogs> sickLogs;
+
 set<int> sickPeople;
+
+bool rottenCheese[51] = {false, };
+
+bool ateCheese(int cheese, int sick_p, int sick_t) {
+    for (int i = 0; i < D; i++) {
+        if(eatLogs[i].m == cheese && eatLogs[i].p == sick_p  && eatLogs[i].t < sick_t) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
     cin >> N >> M >> D >> S;
@@ -44,33 +36,44 @@ int main() {
     for (int i = 0; i < D; i++) {
         int p, m, t;
         cin >> p >> m >> t;
-        eatDatas[p][m] = max(eatDatas[p][m], t);
+        eatLogs.push_back({p, m, t});
     }
 
     for (int i = 0; i < S; i++) {
         int sick_p, sick_t;
         cin >> sick_p >> sick_t;
-        sickDatas.push_back({sick_p, sick_t});
+        sickLogs.push_back({sick_p, sick_t});
     }
 
-    for (int i = 0; i < S; i++) {
-        int p = sickDatas[i].p;
-        int t = sickDatas[i].t;
+    // 상한 치즈를 추론하는 과정
+    for (int i = 1; i <= 50; i++) {
+        // i번 치즈가 상했다 가정함
+        bool rotten = true;
+        for (int j = 0; j < S; j++) {
+            // 아픈 모든 사람들이 아프기 이전에 i번 치즈를 먹었는지 확인함
+            int p = sickLogs[j].p;
+            int t = sickLogs[j].t;
 
-        for (int j = 1; j <= M; j++) {
-            if (eatDatas[p][j] > 0 && eatDatas[p][j] < t) {
-                rottenCheese.insert(j);
+            // 만약 한명이라도 치즈를 아프기전 먹지 않았다면 상한 것이 아님
+            if(!ateCheese(i, p, t)) {
+                rotten = false;
+                break;
             }
+        }
+
+        if(rotten) {
+            rottenCheese[i] = true;
         }
     }
 
     // 상한 치즈를 먹은 전체 인원 계산
-    for (auto cheese : rottenCheese) {
-        for (int i = 1; i <= N; i++) {
-            if (eatDatas[i][cheese] > 0) {
-                sickPeople.insert(i);
+    for (int i = 1; i <= N; i++) {
+        if (rottenCheese[i]) {
+            for (auto eatLog : eatLogs) {
+                if (eatLog.m == i) sickPeople.insert(eatLog.p);
             }
         }
+        
     }
 
     cout << sickPeople.size();
