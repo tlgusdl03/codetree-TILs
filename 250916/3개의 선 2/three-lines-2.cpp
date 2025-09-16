db159e3c0af42362d683f5cffed5cca11c6c5059
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-16 14:58:19 +0900

    Title: 3개의 선 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250916/3개의 선 2/three-lines-2.cpp
+++ 250916/3개의 선 2/three-lines-2.cpp
@@ -1,35 +1,67 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int n;
 int x[20], y[20];
+int cntX[10], cntY[10];
 
-bool getResult (int a, int b, int c) {
-    int x1 = x[a];
-    int y1 = y[a];
-    int x2 = x[b];
-    int y2 = y[b];
-    int x3 = x[c];
-    int y3 = y[c];
+int permutation[8][3] = {
+    {0, 0, 0},
+    {0, 0, 1},
+    {0, 1, 0},
+    {0, 1, 1},
+    {1, 0, 0},
+    {1, 0, 1},
+    {1, 1, 0},
+    {1, 1, 1}
+};
 
-    for (int i = 0; i < n; i++) {
-        if (x[i] != x1 && x[i] != x2 && x[i] != x3 && y[i] != y1 && y[i] != y2 && y[i] != y3) return false;
+bool getResult(int a, int b, int c) {
+    int arrX[3] = {x[a], x[b], x[c]};
+    int arrY[3] = {y[a], y[b], y[c]};
+
+    for (int perm = 0; perm < 8; perm++) {
+        bool allCovered = true;
+        for (int i = 0; i < n; i++) {
+            bool covered = false;
+            for (int j = 0; j < 3; j++) {
+                if (permutation[perm][j] == 1) {
+                    if (x[i] == arrX[j]) {
+                        covered = true;
+                        break;
+                    }
+                }
+                else {
+                    if (y[i] == arrY[j]) {
+                        covered = true;
+                        break;
+                    }
+                }
+            }
+            if (!covered) {
+                allCovered = false;
+                break;
+            }
+        }
+        if (allCovered) return true;
     }
 
-    return true;
+    return false;
 }
 
 int main() {
     cin >> n;
 
-    if (n <= 3) {
+    if (n < 4) {
         cout << 1;
         return 0;
     }
 
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> y[i];
+        cntX[x[i]]++;
+        cntY[y[i]]++;
     }
 
     for (int i = 0; i < n - 2; i++) {
