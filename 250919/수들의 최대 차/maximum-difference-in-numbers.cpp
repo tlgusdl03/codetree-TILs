ec8bc93d600b7feb8c40f26341d4e3dbd9c4e166
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-19 12:37:15 +0900

    Title: 수들의 최대 차, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250919/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, K;
+int arr[1000];
+int MIN = 10001;
+int MAX = 0;
+
+int main() {
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        MIN = min(MIN, arr[i]);
+        MAX = max(MAX, arr[i]);
+    }
+
+    sort(arr, arr + N);
+
+    int result = 0;
+    for (int i = MIN; i <= MAX - K; i++) {
+        int temp = 0;
+        for (int j = 0; j < N; j++) {
+            if (i <= arr[j] && arr[j] <= i + K) temp++;
+        }
+        result = max(result, temp);
+    }
+
+    cout << result;
+    return 0;
+}
