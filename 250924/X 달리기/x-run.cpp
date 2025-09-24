8251f5cb69df5718613d2122b94695eee2c3e30e
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-24 17:18:49 +0900

    Title: X 달리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250924/X 달리기/x-run.cpp
+++ 250924/X 달리기/x-run.cpp
@@ -6,23 +6,23 @@ int X;
 
 int getResult() {
     int time = 0;
-    int speed = 1;
+    int speed = 0;
     int distance = 0;
-    int remain_distance = X;
 
-    while (true) {
-        time++;
+    while (distance < X) {
+        int remain_X = X - distance;
+
+        if (remain_X >= ((speed + 1) * (speed + 2)) / 2) speed++;
+        else if (remain_X < (speed * (speed + 1)) / 2) speed--;
+
         distance += speed;
-        remain_distance = X - distance;
 
-        if (distance >= X) break;
-
-        if ((speed + distance) < remain_distance) speed++;
-        else if (speed > 1) speed--;
+        time++;
     }
-
+    
     return time;
 }
+
 
 int main() {
     cin >> X;
