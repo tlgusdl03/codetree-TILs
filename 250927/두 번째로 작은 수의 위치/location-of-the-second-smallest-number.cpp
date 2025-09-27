8f9aa4dbc45803cd9c58e50ac4b7f0192ba2569a
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-27 21:26:40 +0900

    Title: 두 번째로 작은 수의 위치, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250927/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
+++ 250927/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int n;
-int a[100];
+int a[101];
 
 int main() {
     cin >> n;
@@ -12,33 +12,33 @@ int main() {
     }
 
     int first = 101;
-    int second = 101;
-    int idx = 0;
-    int count = 0;
 
     for (int i = 1; i <= n; i++) {
         first = min(first, a[i]);
     }
 
     for (int i = 1; i <= n; i++) {
-        if (first == a[i]) {
-            a[i] = 101;
+        if (a[i] == first) {
+            a[i] = 1000001;
         }
     }
 
+    int second = 101;
     for (int i = 1; i <= n; i++) {
         second = min(second, a[i]);
     }
 
+    int idx = -1;
+    int count = 0;
     for (int i = 1; i <= n; i++) {
-        if (second == a[i]) {
+        if (a[i] == second) {
             idx = i;
             count++;
         }
     }
 
-    if (count >= 2 || count == 0) {
-        cout << -1;
+    if (count == 0 || count > 1) {
+        cout << "-1";
     }
     else {
         cout << idx;
