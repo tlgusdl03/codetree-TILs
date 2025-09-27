bf840aa0296d96e8527b615d4c205fbd2401bfc7
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-09-27 22:07:51 +0900

    Title: L, R 그리고 B, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250927/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+char board[10][10];
+
+int main() {
+
+    int L_pos[2];
+    int R_pos[2];
+    int B_pos[2];
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cin >> board[i][j];
+
+            if (board[i][j] == 'L') {
+                L_pos[0] = i;
+                L_pos[1] = j;
+            }
+            else if (board[i][j] == 'R') {
+                R_pos[0] = i;
+                R_pos[1] = j;
+            }
+            else if (board[i][j] == 'B') {
+                B_pos[0] = i;
+                B_pos[1] = j;
+            }
+        }
+    }
+
+    // L B R이 같은 행, 열에 있는 경우 +2를
+    // L B R이 다른 행, 열에 있는 경우 가로 + 세로
+
+    int result = 0;
+
+    if (L_pos[0] == R_pos[0] && L_pos[0] == B_pos[0] && R_pos[0] == B_pos[0]) {
+        result = abs(B_pos[0] - L_pos[0]) + 2;
+    }
+    else if (L_pos[1] == R_pos[1] && L_pos[1] == B_pos[1] && R_pos[1] == B_pos[1]) {
+        result = abs(B_pos[1] - L_pos[1]) + 2;
+    }
+    else {
+        result = abs(B_pos[0] - L_pos[0]) + abs(B_pos[1] - L_pos[1]) - 1;
+    }
+
+    cout << result;
+
+    return 0;
+}
