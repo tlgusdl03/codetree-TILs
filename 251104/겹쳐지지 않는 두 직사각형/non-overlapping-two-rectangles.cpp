adab174fb00eafe901ec4f1b4836f8b6d096b110
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-11-04 11:54:44 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251104/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[5][5];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            for (int k = i; k )
+        }
+    }
+
+    return 0;
+}
