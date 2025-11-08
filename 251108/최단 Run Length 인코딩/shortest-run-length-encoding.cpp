13c3884ecc95a8b7d94b8cc86d87365f9cc704dc
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-11-08 19:05:37 +0900

    Title: 최단 Run Length 인코딩, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251108/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string A;
+
+int findLen(string A) {
+    int len = A.length();
+    string result = "";
+
+    char last = A[0];
+    int count = 1;
+    for (int i = 1; i < len; i++) {
+        if (A[i] == last) {
+            count++;
+        }
+        else {
+            result += last;
+            result += to_string(count);
+            last = A[i];
+            count = 1;
+        }
+        
+    }
+
+    result += last;
+    result += to_string(count);
+
+    return result.length();
+}
+
+string rotate(string A) {
+    int len = A.length();
+    char temp = A[len - 1];
+
+    for (int j = len - 1; j > 0; j--) {
+        A[j] = A[j - 1];
+    }
+
+    A[0] = temp;
+
+    return A;
+}
+
+int findMinLen(string A) {
+    int result = 11;
+    int len = A.length();
+
+    for (int i = 0; i < len; i++) {
+        
+        string rotated_A = rotate(A);
+
+        A = rotated_A;
+
+        result = min(result, findLen(rotated_A));
+    }
+
+    return result;
+}
+
+int main() {
+    cin >> A;
+
+    int result = findMinLen(A);
+
+    cout << result;
+
+    return 0;
+}
