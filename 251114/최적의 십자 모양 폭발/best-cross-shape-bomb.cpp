9f2034393a4d666f1a8ef0efb5965744215827ee
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-11-14 12:28:36 +0900

    Title: 최적의 십자 모양 폭발, Time: 40ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251114/최적의 십자 모양 폭발/best-cross-shape-bomb.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int grid[50][50];
+int backup[50][50];
+
+void bomdAndFall(int row, int col) {
+    int len = grid[row][col];
+
+    int curX = row;
+    int curY = col;
+    for (int i = 0; i < len; i++) {
+        grid[curX][curY] = 0;
+        curY--;
+        if (curY < 0) break;
+    }
+
+    curX = row;
+    curY = col;
+    for (int i = 0; i < len; i++) {
+        grid[curX][curY] = 0;
+        curX--;
+        if (curX < 0) break;
+    }
+
+    curX = row;
+    curY = col;
+    for (int i = 0; i < len; i++) {
+        grid[curX][curY] = 0;
+        curY++;
+        if (curY >= n) break;
+    }
+
+    curX = row;
+    curY = col;
+    for (int i = 0; i < len; i++) {
+        grid[curX][curY] = 0;
+        curX++;
+        if (curX >= n) break;
+    }
+
+    for (int j = 0; j < n; j++) {
+        int temp[50] = {0, };
+        int idx = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (grid[i][j] != 0) {
+                temp[idx] = grid[i][j];
+                idx--;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            grid[i][j] = temp[i];
+        }
+    }
+}
+
+int countPair() {
+    int result = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int cur = grid[i][j];
+
+            if (cur == 0) continue;
+
+            if (i + 1 < n && cur == grid[i + 1][j]) {
+                if (i + 2 >= n || cur != grid[i + 2][j]) result++;
+            }
+
+            if (j + 1 < n && cur == grid[i][j + 1]) {
+                if (j + 2 >= n || cur != grid[i][j + 2]) result++;
+            }
+        }
+    }
+
+    return result;
+}
+
+void restore() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            grid[i][j] = backup[i][j];
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+            backup[i][j] = grid[i][j];
+        }
+    }
+
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            bomdAndFall(i, j);
+            result = max(result, countPair());
+            restore();
+        }
+    }
+
+    cout << result;
+    return 0;
+}
