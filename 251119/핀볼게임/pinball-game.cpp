aa10301dd5f69245394cbb3854a0dd7d376fdb9f
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-11-19 11:52:33 +0900

    Title: 핀볼게임, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251119/핀볼게임/pinball-game.cpp
+++ 251119/핀볼게임/pinball-game.cpp
@@ -109,8 +109,8 @@ int solve() {
 int main() {
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) cin >> grid[i][j];
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) cin >> grid[i][j];
 
     cout << solve();
 
