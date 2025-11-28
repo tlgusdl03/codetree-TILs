5f285c6bfc45dd5dc1f2500aa06add218703d249
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-11-28 12:53:08 +0900

    Title: 알파벳과 사칙연산, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251128/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 251128/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -4,46 +4,44 @@ using namespace std;
 
 string expression;
 
-int input_Arr[200] = {0, };
+int input_Arr[6] = {0, };
 int result = INT_MIN;
 
-void solve(int idx) {
-    if (idx == expression.length()) {
-        int a = input_Arr[0];
-        char oper;
-        int b = 0;
+int calc() {
+    int n = expression.length();
+    int value = input_Arr[expression[0] - 'a'];
+    char oper = 0;
 
-        for (int i = 1; i < expression.length(); i++) {
-            if ('a' <= expression[i] && expression[i] <= 'f') {
-                b = input_Arr[i];
-                if (oper == '+') {
-                    a += b;
-                }
-                else if (oper == '-') {
-                    a -= b;
-                }
-                else if (oper == '*') {
-                    a *= b;
-                }
-            }
-            else {
-                oper = expression[i];
-            }
+    for (int i = 1; i < n; i++) {
+        char ch = expression[i];
+
+        if ('a' <= ch && ch <= 'f') {
+            int idx = ch - 'a';
+            int v = input_Arr[idx];
+            if (oper == '+') value += v;
+            else if (oper == '-') value -= v;
+            else if (oper == '*') value *= v;
         }
+        else {
+            oper = ch;
+        }
+    }
+
+    return value;
+}
+
+void solve(int idx) {
+    if (idx == 6) {
+        int a = calc();
         result = max(result, a);
     }
     else {
-        if ('a' <= expression[idx] && expression[idx] <= 'f') {
-            for (int i = 1; i <= 4; i++) {
-                input_Arr[idx] = i;
-                solve(idx + 1);
-            }
-            input_Arr[idx] = 0;
-        }
-        else {
+        for (int i = 1; i <= 4; i++) {
+            input_Arr[idx] = i;
             solve(idx + 1);
-        }    
-    }
+            input_Arr[idx] = 0;
+        } 
+    }  
 }
 
 int main() {
