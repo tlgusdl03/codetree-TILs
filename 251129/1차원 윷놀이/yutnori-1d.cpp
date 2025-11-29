8cce9679c6514a19e1daf53bfe0369ef1f7c9526
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-11-29 18:54:04 +0900

    Title: 1차원 윷놀이, Time: 207ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251129/1차원 윷놀이/yutnori-1d.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, k;
+int nums[12];
+int location[4] = {0, };
+int result = 0;
+
+void solve(int idx) {
+
+    int temp = 0;
+    for (int i = 0; i < k; i++) {
+        if (location[i] >= m - 1) {
+            temp++;
+        }
+    }
+
+    result = max(result, temp);
+
+    if (idx == n) {
+        return;
+    }
+    else {
+        for (int i = 0; i < k; i++) {
+            if (location[i] < m) {
+                location[i] += nums[idx];
+
+                solve(idx + 1);
+
+                location[i] -= nums[idx];
+            }
+        }
+    }
+}
+int main() {
+    cin >> n >> m >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    solve(0);
+
+    cout << result << '\n';
+
+    return 0;
+}
