b57f39d272b92dc4957a51eb53ecb694ebfd927d
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-12-18 13:20:17 +0900

    Title: 비를 피하기, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 251218/비를 피하기/stay-out-of-rain.cpp
+++ 251218/비를 피하기/stay-out-of-rain.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <queue>
 #include <cstring>
+#include <algorithm>
+#define MAX_DISTANCE 10001
 using namespace std;
 
 int n, h, m;
 int grid[100][100];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
-bool visited[100][100];
 int answer[100][100];
-vector<pair<int, int>> people;
+bool visited[100][100];
+vector<pair<int, int>> shelter;
 
 struct Node {
     int row, col, step;
@@ -20,19 +22,25 @@ bool isInRange(int row, int col) {
     return (0 <= row && row < n && 0 <= col && col < n);
 }
 
-int BFS(int row, int col) {
+void BFS() {
     queue<Node> q;
-    visited[row][col] = true;
-    q.push({row, col, 0});
+    memset(visited, false, sizeof(visited));
 
+    for (int i = 0; i < shelter.size(); i++) {
+        int r = shelter[i].first;
+        int c = shelter[i].second;
+        visited[r][c] = true;
+        q.push({r, c, 0});
+    }
+    
     while(!q.empty()) {
         int cur_R = q.front().row;
         int cur_C = q.front().col;
         int cur_S = q.front().step;
         q.pop();
 
-        if (grid[cur_R][cur_C] == 3) {
-            return cur_S;
+        if (grid[cur_R][cur_C] == 2) {
+            answer[cur_R][cur_C] = min(answer[cur_R][cur_C], cur_S);
         }
 
         for (int i = 0; i < 4; i++) {
@@ -40,31 +48,30 @@ int BFS(int row, int col) {
             int next_C = cur_C + dy[i];
             int next_S = cur_S + 1;
 
-            if (isInRange(next_R, next_C) && !visited[next_R][next_C] && grid[next_R][next_C] != 1) {
+            if (isInRange(next_R, next_C) 
+            && grid[next_R][next_C] != 1 
+            && !visited[next_R][next_C]) {
+                visited[next_R][next_C] = true;
                 q.push({next_R, next_C, next_S});
             }
         }
     }
-
-    return -1;
 }
 
 void solve() {
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (grid[i][j] != 2) {
-                answer[i][j] = 0;
-            }
+            answer[i][j] = MAX_DISTANCE;
         }
     }
 
-    for (int i = 0; i < people.size(); i++) {
-        memset(visited, false, sizeof(visited));
-        int r = people[i].first;
-        int c = people[i].second;
-        int result = BFS(r, c);
-        answer[r][c] = result;
+    BFS();
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] != 2) answer[i][j] = 0;
+            if (answer[i][j] == MAX_DISTANCE) answer[i][j] = -1;
+        }
     }
 
     for (int i = 0; i < n; i++) {
@@ -81,8 +88,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> grid[i][j];
-            if (grid[i][j] == 2) {
-                people.push_back({i, j});
+            if (grid[i][j] == 3) {
+                shelter.push_back({i, j});
             }
         }
     }
