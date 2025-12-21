be61d786d6f6bd38e2c80405fe15fcb04f8c6c30
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2025-12-21 17:22:56 +0900

    Title: 계단 오르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251221/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int tb[1001];
+int n;
+
+int main() {
+    cin >> n;
+
+    tb[0] = 0;
+    tb[1] = 0;
+    tb[2] = 1;
+    tb[3] = 1;
+
+    for (int i = 4; i < 1000; i++) {
+        tb[i] = (tb[i - 2] + tb[i - 3]) & 10007;
+    }
+
+    cout << tb[n] << '\n';
+
+    return 0;
+}
