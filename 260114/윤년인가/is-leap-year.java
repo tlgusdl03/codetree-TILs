6e70c5a214b393cf552123aeb90b240ac8d253f5
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2026-01-14 21:29:23 +0900

    Title: 윤년인가, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260114/윤년인가/is-leap-year.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int Y = sc.nextInt();
+
+        if (Y % 100 == 0 && Y % 400 != 0) System.out.print("false");
+        
+        else if (Y % 4 == 0) System.out.print("true");
+        else System.out.print("false");
+    }
+}
