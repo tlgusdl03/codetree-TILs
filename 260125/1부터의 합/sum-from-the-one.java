276221c56be3f9b42183b4a2d1d15203e8624162
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2026-01-25 17:47:22 +0900

    Title: 1부터의 합, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260125/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+
+            if (sum >= N) {
+                System.out.print(i);
+                break;
+            }
+        }
+
+        sc.close();
+    }
+}
