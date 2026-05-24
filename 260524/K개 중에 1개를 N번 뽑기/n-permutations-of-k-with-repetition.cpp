a779410d86abb5660304e799dfe07b9ac549e8fc
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2026-05-24 15:37:54 +0900

    [Passed] K개 중에 1개를 N번 뽑기 (50ms, 1MB)

--- 260524/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.cpp
+++ 260524/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int K, N;
 int btr[8];
 
-void backTracking(int k, int n) {
+void backTracking(int n) {
     // 숫자를 하나 고르는 행위를 N번 반복하였다면
     if (n == N) {
         // 뽑았던 수를 출력하고 더 이상 진행하지 않음
@@ -19,14 +19,14 @@ void backTracking(int k, int n) {
     // 1이상 K이하의 수를 하나 고른 후 다음 단계로 진행
     for (int i = 1; i <= K; i++) {
         btr[n] = i;
-        backTracking(i, n + 1);
+        backTracking(n + 1);
     }
 }
 
 int main() {
     cin >> K >> N;
     
-    backTracking(1, 0);
+    backTracking(0);
 
     return 0;
 }
