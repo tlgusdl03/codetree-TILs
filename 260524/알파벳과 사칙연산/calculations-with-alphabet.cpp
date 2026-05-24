877bfdd6bca0ab87573145fc96b60d58e6e12408
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2026-05-24 16:34:11 +0900

    [Passed] 알파벳과 사칙연산 (2ms, 0MB)

--- 260524/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 260524/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -4,15 +4,19 @@
 #define MAX_OPERRAND 6
 using namespace std;
 
+// climits 헤더를 사용하여 답을 정수의 최솟값으로 초기화 시킴
 int ans = INT_MIN;
+// a ~ e까지에 들어갈 수를 고르기 위해 배열을 선언함
 int btr[6];
 string str;
 int N;
 
+// 알파벳에 해당하는 고른 값을 반환함
 int trans(char a) {
     return btr[a - 'a'];
 }
 
+// 알파벳, 연산자, 알파벳, ... , 연산자, 알파벳의 순서로 들어온 식에 접근하여 계산을 진행함
 void calculator() {
     int result = trans(str[0]);
     for (int i = 1; i < N; i += 2) {
@@ -29,6 +33,7 @@ void calculator() {
     ans = max(ans, result);
 }
 
+// a ~ e까지 대입할 수를 고름
 void backTracking(int cnt) {
     if (cnt == MAX_OPERRAND) {
         calculator();
