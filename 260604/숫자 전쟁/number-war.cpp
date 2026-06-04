5906b8f99b081954b51b3e48758c484c0d5b7020
Lee Si Hyeon <112377618+tlgusdl03@users.noreply.github.com>
2026-06-04 15:50:28 +0900

    [Passed] 숫자 전쟁 (2ms, 0MB)

---
+++ 260604/숫자 전쟁/number-war.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n;
+int first_cards[1000];
+int second_cards[1000];
+int dp[1001][1001];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> first_cards[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> second_cards[i];
+    }
+
+    for (int i = 0 ; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = -1;
+        }
+    }
+
+    dp[0][0] = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dp[i][j] == -1) continue;
+
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j]);
+
+            if (first_cards[i] > second_cards[j]) {
+                dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + second_cards[j]);
+            }
+            else if (first_cards[i] < second_cards[j]) {
+                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+            }
+        }
+    }
+    
+    int answer = 0;
+
+    for (int i = 0; i <= n; i++) {
+        answer = max(answer, dp[i][n]);
+    }
+
+    for (int j = 0; j <= n; j++) {
+        answer = max(answer, dp[n][j]);
+    }
+
+    cout << answer;
+
+    return 0;
+}
+
